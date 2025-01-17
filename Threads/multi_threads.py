f760e9fb84749d2d89c3d65a87be46f20bb21e76
Marcelo Ezequiel <marcelogustavo05@hotmail.com>
2025-01-17 16:26:05 -0300

    feat(multi_threads): cenário onde o produtor produz mais rápido que o consumidor.

--- Threads/multi_threads.py
+++ Threads/multi_threads.py
@@ -3,23 +3,32 @@ import queue
 import time
 
 def produtor(fila):
-    for i in range(5):
-        time.sleep(1)  
+    for i in range(20):
+        time.sleep(0.5)  
         mensagem = f"Mensagem {i}"
         fila.put(mensagem)
         print(f"Produzido: {mensagem}")
-def consumidor(fila):
-    while True:
-        mensagem = fila.get()
-        print(f"Consumido: {mensagem}")
-        fila.task_done()
 
-fila = queue.Queue()
+def consumidor(fila):
+    while fila.not_empty:
+        time.sleep(100) 
+        mensagem = fila.get()        
+        print(f"Consumido: {mensagem}")
+
+fila = queue.Queue(10)
+
 produtor_thread = threading.Thread(target=produtor, args=(fila,))
 consumidor_thread = threading.Thread(target=consumidor, args=(fila,))
+
+print("Thread produtor iniciada")
 produtor_thread.start()
+print("Thread consumidor iniciada")
 consumidor_thread.start()
+
 produtor_thread.join()
+print("Thread produtor encerrada")
+
 consumidor_thread.join()
+print("Thread consumidora encerrada")
 
 print("Threads finalizadas")
